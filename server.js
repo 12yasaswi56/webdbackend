0e9a7e73af203833c5859083b789ebddfb89389e
12yasaswi56 <yasaswikopparapu624@gmail.com>
2025-03-22 12:21:12 +0530

    fc

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use('/uploads', express.static('uploads'));
 // ✅ Set up Socket.IO with CORS
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://social-frontend-five.vercel.app/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true
